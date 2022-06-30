4efab89b2ee4be7ff4d29ae9bde19d0c85121fd6
Rami <103028944+Rami24t@users.noreply.github.com>
2022-06-30 13:18:39 +0200

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,35 @@
+let snackServings = 4;
+let guests = 2;
+console.log(snackServings + guests);
+console.log(snackServings-guests);
+console.log(guests-snackServings);
+
+console.log(snackServings*guests);
+
+console.log(snackServings/guests);
+
+//5
+let drinks=18;
+console.log(resultOne = snackServings*guests/drinks);
+
+//6
+let monkeys = 15;
+let bananas = 9;
+console.log(monkeys/bananas);
+
+//7
+let monkeyfights = 20;
+console.log((resultTwo= monkeys+bananas)*monkeyfights);
+
+//8
+let monkeyBusiness = 90;
+monkeyBusiness +=1;
+let boredom = 13;
+boredom -=1;
+
+//10
+let happiness = monkeys - bananas;
+console.log(happiness +monkeyfights);
+
+//11
+console.log(resultOne/resultTwo);
